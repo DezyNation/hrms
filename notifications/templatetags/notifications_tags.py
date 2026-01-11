be1df0a01f0cb8463874f47209dcb6cc7d927ed4
Dezynation Digital Labs <87603422+DezyNation@users.noreply.github.com>
2026-01-11 10:56:43 +0530

    Use mark_safe for HTML in notifications_tags
    Change return type to mark_safe for HTML output.

--- notifications/templatetags/notifications_tags.py
+++ notifications/templatetags/notifications_tags.py
@@ -4,6 +4,7 @@ from django import get_version
 from django.template import Library
 from django.urls import reverse
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 register = Library()
 
@@ -78,7 +79,7 @@ def live_notify_badge(context, badge_class="live_notify_badge"):
     html = "<span class='{badge_class}'>{unread}</span>".format(
         badge_class=badge_class, unread=user.notifications.unread().count()
     )
-    return format_html(html)
+    return mark_safe(html)
 
 
 @register.simple_tag
